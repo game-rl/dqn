0ccc11d14b792ae354d6b6e41f5b9a5b8d89887f
pi.tau <pi.tau@yahoo.com>
2025-01-20 17:53:18 +0100

    update the docstrings

--- gamerl/dqn/jax/dqn.py
+++ gamerl/dqn/jax/dqn.py
@@ -14,47 +14,53 @@ import numpy as np
 from tqdm import tqdm
 
 
-Key = Any       # Key for a pseudo random number generator (PRNG)
-PyTree = Any    # PyTrees are arbitrary nests of ``jnp.ndarrays``
-OptState = Any  # Arbitrary object holding the state of the optimizer.
+Key = Any
+"""Key for a pseudo random number generator (PRNG)."""
 
-# DeepQNetwork func takes the model parameters and the observations as input and
-# returns the state-action values ``Q(s, a)`` for each (observation, action) pair.
+PyTree = Any
+"""PyTrees are arbitrary nests of ``jnp.ndarrays``."""
+
+OptState = Any
+"""Arbitrary object holding the state of the optimizer."""
+
+# DeepQNetwork func takes as input the model parameters and a batch
+# of observations, and returns the state-action values ``Q(s, a)``
+# for each (observation, action) pair.
 DeepQNetwork = Callable[[PyTree, ArrayLike], jax.Array]
 
-# OptimizerFn takes parameters, their gradients, and the
-# optimizer state as input and returns the updated parameters and
-# the new state.
+# OptimizerFn takes as input parameters, their gradients, and the
+# optimizer state and returns the updated parameters and the new state.
 OptimizerFn = Callable[[PyTree, PyTree, OptState], tuple[PyTree, OptState]]
 
-# EnvironmentStepFn is a step function for a vectorized environment
-# conforming to the Gymnasium environments API. See:
+# EnvironmentStepFn is a step function for a vectorized
+# environment conforming to the Gymnasium environments API. See:
 #   https://gymnasium.farama.org/api/env/#gymnasium.Env.step
 #   https://gymnasium.farama.org/api/vector/#gymnasium.vector.VectorEnv.step
 #
-# The function takes as input a batch of actions to update the environment state,
-# and returns the next observations and the rewards resulting from the actions.
-# The function also returns boolean arrays indicating whether any of the
-# sub-environments were terminated or truncated and an info dict.
+# The function takes as input a batch of actions to update the
+# environment state, and returns the next observations and the
+# rewards resulting from the actions. The function also returns
+# boolean arrays indicating whether any of the sub-environments
+# were terminated or truncated, as well as an info dict.
 #
 # If ``None`` is given as input, then the function returns the
-# observations for the current state of the environment.
+# current observations without stepping the environment.
 EnvironmentStepFn = Callable[
     [ArrayLike | None],
     tuple[jax.Array, jax.Array, jax.Array, jax.Array, dict],
 ]
 
 # Transitions is a tuple (o, a, r, o_next, d) of nd-arrays containing:
-#   - a batch of the current observations ``o``;
+#   - batch of observations ``o``;
 #   - the selected actions ``a`` for each observation;
 #   - the obtained rewards ``r``;
 #   - the next observations ``o_next``;
-#   - a boolean flag ``d`` indicating if ``o_next`` was terminal.
+#   - boolean flags ``d`` indicating which ``o_next`` are terminal.
 Transitions = tuple[ArrayLike, ArrayLike, ArrayLike, ArrayLike, ArrayLike]
 
 # ReplayBuffer is a container for storing and sampling transitions.
 # Implementations might include strategies for prioritized sampling of
-# transitions or smart eviction of old experiences and others.
+# transitions, smart eviction of old experiences, and others.
 class ReplayBuffer(Protocol):
     def store(self, ts: Transitions) -> None:
         """Store a batch of transitions in the buffer possibly overwriting old ones."""
@@ -65,23 +71,49 @@ class ReplayBuffer(Protocol):
     def __len__(self) -> int:
         """Return the number of transitions currently stored in the buffer."""
 
+# DQNTrainer is a callable that trains an agent to maximize
+# expected return from a given environment.
 @dataclass
 class DQNTrainer:
-    """
-        ```dqn_trainer = DQNTrainer(**kwargs)```
-        ```params, opt_state = dqn_trainer(rng, params, opt_state) ```
-    """
+    """DQNTrainer trains a Q-network for multiple iterations.
+
+	Each training iteration consists of two stages:
+	  1. Data collection stage. The environment is stepped (possibly
+        multiple times) and the transitions are stored in a replay buffer;
+	  2. Parameter optimization stage. The parameters of the q-network
+        are optimized using a one-step TD target.
+
+	The q-network and the environment are provided to the trainer upon
+	initialization. The trainer can then be called multiple times to
+	update the network parameters. You can see how well the current
+    q-network is performing in between calling the trainer:
+
+	```python
+	# Initialize the trainer.
+	dqn_trainer = DQNTrainer(q_fn, optim_fn, env_fn, **kwargs)
+
+	# Call the trainer to update the current params.
+	params, opt_state = dqn_trainer(rng1, params, opt_state, n_iters)
+
+	# Test the agent after training.
+	record_demo(env_fn, q_fn, params)
+
+	# Train the agent some more.
+	params, opt_state = dqn_trainer(rng2, params, opt_state, n_iters)
+	```
+	"""
+
     q_fn: DeepQNetwork
     optim_fn: OptimizerFn
     env_fn: EnvironmentStepFn
     replay_buffer: ReplayBuffer
-    discount: float = 1.
-    n_steps: int = 1
-    n_updates: int = 1
-    update_tgt: int = 1
-    batch_size: int = 64
-    huber_delta: float = 1.
-    eps: Callable[[int], float] = lambda x: 0.05
+    discount: float = 1.    # discount for future rewards
+    n_steps: int = 1        # number of steps before updating
+    n_updates: int = 1      # number of updates
+    update_tgt: int = 1     # how often to update the target net
+    batch_size: int = 64    # batch size for iterating over the replay buffer
+    huber_delta: float = 1. # bound for the huber loss transform
+    eps: Callable[[int], float] = lambda x: 0.05 # schedule for epsi-greedy
     train_log: dict[str, list[float]] = field( # for logging info during training
         default_factory=lambda: defaultdict(list), init=False)
 
@@ -92,18 +124,13 @@ class DQNTrainer:
         opt_state: OptState,
         n_iters: int
     ) -> tuple[PyTree, OptState]:
-        """Run the DQN trainer for multiple iterations to update the q-network
-        parameters. Each iteration consists of two stages:\n
-          1. data collection stage, where we step the environment (possibly
-          multiple times) and store the transitions in the replay buffer;
-          2. parameter optimization stage, where we update the q-network
-          parameters (possibly multiple times) using bootstrapped TD target.
+        """Update the q-network parameters using one-step TD learning.
 
         Args:
             rng: Key
-                A PRNG key array.
+                PRNG key array.
             params: PyTree
-                Current model parameters for the agent function.
+                Current parameters for the q-network.
             opt_state: OptState
                 Current optimizer state for the optimizer function.
             n_iters: int
@@ -138,8 +165,8 @@ class DQNTrainer:
                 with warnings.catch_warnings():
                     # We might make a step without completing any episodes. In
                     # this case we want to store NaN in the history. Taking the
-                    # mean an empty slice throws a runtime warning and returns a
-                    # NaN, which is exactly what we want.
+                    # mean of an empty slice throws a runtime warning and returns
+                    # a NaN, which is exactly what we want.
                     warnings.simplefilter("ignore", category=RuntimeWarning)
                     self.train_log["Episode Returns"].append(np.mean(info["ep_r"]))
                     self.train_log["Episode Lengths"].append(np.mean(info["ep_l"]))
@@ -152,7 +179,7 @@ class DQNTrainer:
                 batch = self.replay_buffer.sample(rng_, self.batch_size)
 
                 # Compute the loss and perform the backward pass.
-                loss, grads = td_loss(
+                loss, grads = td_error(
                     batch, self.q_fn, params, tgt_params, self.discount, self.huber_delta,
                 )
                 params, opt_state = self.optim_fn(params, grads, opt_state)
@@ -165,6 +192,9 @@ class DQNTrainer:
 
         return params, opt_state
 
+# step simulates a single step from the interaction loop between
+# the agent and the vectorized environment and returns the
+# observed transitions.
 def step(
     rng: Key,
     env_fn: EnvironmentStepFn,
@@ -176,7 +206,7 @@ def step(
 
     Args:
         rng: Key
-            A PRNG key array.
+            PRNG key array.
         env_fn: EnvironmentStepFn
             Function for stepping the environment given the actions.
         q_fn: DeepQNetwork
@@ -188,11 +218,11 @@ def step(
 
     Returns:
         Transitions
-            A tuple (o, a, r, o_next, d) of nd-arrays.
+            Tuple (o, a, r, o_next, d) of nd-arrays.
         dict[str, Sequence[float]]
             Info dict.
     """
-    o, *_ = env_fn(None)
+    o, *_ = env_fn(None) # shape (B, *)
 
     # Select the actions using eps-greedy.
     q_values = q_fn(params, o) # shape (B, acts)
@@ -223,17 +253,40 @@ def step(
     return transitions, info
 
 # Differentiate the output of the function with respect to the
-# fourth input parameter, i.e. the parameters of the q-network.
+# third input parameter, i.e. the parameters of the q-network.
 @partial(jax.jit, static_argnames="q_fn")
 @partial(jax.value_and_grad, argnums=2, has_aux=False)
-def td_loss(
+def td_error(
     batch: Transitions,
     q_fn: DeepQNetwork,
     params: PyTree,
     tgt_params: PyTree,
-    discount: float,
+    gamma: float,
     delta: float,
 ) -> jax.Array:
+    """Compute the mean Huber loss based on the one-step TD error.
+
+    ``err = r_t + gamma * max_a' Q(s',a') - Q(s, a)``\n
+    ``loss = 0.5 * err**2 if err < delta else |err|``
+
+    Args:
+        batch: Transitions
+            Tuple (o, a, r, o_next, d) of nd-arrays.
+        q_fn: DeepQNetwork
+            Function for calculating state-action values.
+        params: PyTree
+            The parameters of the Q-function.
+        gamma: float
+            Discount factor for future rewards.
+        delta: float
+            Threshold at which the Huber loss changes from L2-loss
+            to delta-scaled L1-loss.
+
+    Returns:
+        jax.Array
+            Array of size 1 holding the value of the loss.
+    """
+
     obs, acts, rewards, obs_next, done = batch
     B = obs.shape[0]
 
@@ -252,7 +305,7 @@ def td_loss(
     # Calculate the Huber loss.
     # 0.5 * err^2                   if |err| <= d
     # 0.5 * d^2 + d * (|err| - d)   if |err| > d
-    errs = rewards + discount * q_next * ~done - q_preds
+    errs = rewards + gamma * q_next * ~done - q_preds
     abs_errs = jnp.abs(errs)
     quadratic = jnp.minimum(abs_errs, delta)
     # Same as max(abs_errs - delta, 0) but avoids potentially doubling gradient.
